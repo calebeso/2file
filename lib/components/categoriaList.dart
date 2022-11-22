e2d07e2c17269a215b8ccda51d734b3981defef8
William Ricardo Munaretto <william.munaretto@gmail.com>
2022-11-21 22:26:29 -0300

    retirada das bordas e ajustes de dimensões

--- lib/components/categoriaList.dart
+++ lib/components/categoriaList.dart
@@ -4,7 +4,6 @@ import 'package:intl/intl.dart';
 import '../models/documento.dart';
 
 class CategoriaList extends StatelessWidget {
-
   final List<Documento> documentos;
 
   //constructor
@@ -12,107 +11,84 @@ class CategoriaList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  SingleChildScrollView(
-
-    child: Container(
-      height: 480,
-      child: ListView.builder(
-        itemCount: documentos.length,
-        itemBuilder: (ctx, index) {
-          final doc = documentos[index];
-          return Card(
-            color: const Color(0xffDEF1EB),
-            shadowColor: const Color(0xffFE7C3F),
-            margin: const EdgeInsets.all(5),
-            child: Row(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(5),
-                  margin: const EdgeInsets.only(
-                      left: 20,
-                      top: 10,
-                      bottom: 10
-                  ),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      border: Border.all(
-                        color: const Color(0xff000000),
-                        width: 2,
-                    )
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Id: ${doc.id.toString()}',
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14,
-                            color: Colors.black),
-                      ),
-                      Text(
-                        'Nome: ${doc.nome}.',
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14,
-                            color: Colors.black),
-                      ),
-                      Text(
-                        'Criado em: ${DateFormat('d/MMM/y.').format(doc.criadoEm!).toString()}',
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14,
-                            color: Colors.black),
-                      ),
-                    ],
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => print('abrir documento'),
-                  child: Column(
-                    children: [
-                      Container(
-                          child: Image.asset('assets/images/icon_doc.png', height: 55,),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            border: Border.all(
-                              color: const Color(0xff000000),
-                              width: 2,
-                            )
+    return SingleChildScrollView(
+      child: Container(
+        height: 520,
+        child: ListView.builder(
+          itemCount: documentos.length,
+          itemBuilder: (ctx, index) {
+            final doc = documentos[index];
+            return Card(
+              color: const Color(0xffDEF1EB),
+              shadowColor: const Color(0xffFE7C3F),
+              margin: const EdgeInsets.all(5),
+              child: Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    margin:
+                        const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Id: ${doc.id.toString()}',
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                              color: Colors.black),
                         ),
-                        margin: const EdgeInsets.only(
-                          left: 50
+                        Text(
+                          'Nome: ${doc.nome}.',
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                              color: Colors.black),
                         ),
-                      ),
-                    ],
+                        Text(
+                          'Criado em: ${DateFormat('d/MMM/y.').format(doc.criadoEm!).toString()}',
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                              color: Colors.black),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                GestureDetector(
-                  onTap: () => print('excluir ou editar documento'),
-                  child: Column(
-                    children: [
-                      Container(
-                        child: Image.asset('assets/images/reticencias_vert.png', height: 55,),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            border: Border.all(
-                              color: const Color(0xff000000),
-                              width: 2,
-                            )
+                  GestureDetector(
+                    onTap: () => print('abrir documento'),
+                    child: Column(
+                      children: [
+                        Container(
+                          child: Image.asset(
+                            'assets/images/icon_doc.png',
+                            height: 55,
+                          ),
+                          margin: const EdgeInsets.only(left: 50),
                         ),
-                        margin: const EdgeInsets.only(
-                            left: 20
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ], //children end
-            ),
-          );
-        },
+                  GestureDetector(
+                    onTap: () => print('excluir ou editar documento'),
+                    child: Column(
+                      children: [
+                        Container(
+                          child: Image.asset(
+                            'assets/images/reticencias_vert.png',
+                            height: 55,
+                          ),
+                          margin: EdgeInsets.fromLTRB(50, 20, 5, 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                ], //children end
+              ),
+            );
+          },
+        ),
       ),
-    ),
     );
   }
 }
