0723924f67a41fc371199b0c8dfa560e4209d52f
calebeso <calebeso@MacBook-Air-de-Izabella.local>
2022-12-07 12:48:10 -0300

    feat : ajustando modelos de categorias e documentos, criando tomap e mapfrom para mapear objetos

--- lib/models/categoria.dart
+++ lib/models/categoria.dart
@@ -1,9 +1,24 @@
-class Categoria{
+class Categoria {
+  int? id;
+  String? nome;
+  String? nome_icone;
+  DateTime? criadoEm;
 
-    int? id;
-    String? nome;
-    String? nome_icone;
-    DateTime? timestamp;
+  Categoria({this.id, this.nome, this.nome_icone, this.criadoEm});
 
-    Categoria({this.nome, this.nome_icone, this.timestamp});
+  factory Categoria.fromMap(Map<String, dynamic> json) => new Categoria(
+        id: json['id'],
+        nome: json['nome'],
+        nome_icone: json['nome_icone'],
+        criadoEm: json['criadoEm'],
+      );
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'nome': nome,
+      'nome_icone': nome_icone,
+      'criadoEm': criadoEm,
+    };
+  }
 }
--- lib/models/documento.dart
+++ lib/models/documento.dart
@@ -1,13 +1,38 @@
 import 'categoria.dart';
 
-class Documento{
-
+class Documento {
   int? id;
   String? nome;
+  DateTime? dataCompetencia;
+  DateTime? dataValidade;
   DateTime? criadoEm;
+  int? categoria_id;
 
+  Documento(
+      {this.id,
+      this.nome,
+      this.dataCompetencia,
+      this.dataValidade,
+      this.criadoEm,
+      this.categoria_id});
 
-  Documento({this.id, this.nome, this.criadoEm});
+  factory Documento.fromMap(Map<String, dynamic> json) => new Documento(
+        id: json['id'],
+        nome: json['nome'],
+        dataCompetencia: json['dataCompetencia'],
+        dataValidade: json['dataValidade'],
+        criadoEm: json['criadoEm'],
+        categoria_id: json['categoria_id'],
+      );
 
-
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'nome': nome,
+      'dataCompetencia': dataCompetencia?.microsecondsSinceEpoch,
+      'dataValidade': dataValidade?.microsecondsSinceEpoch,
+      'criadoEm': criadoEm?.microsecondsSinceEpoch,
+      'categoria_id': categoria_id,
+    };
+  }
 }
