b7b65f650bfd5bb2655d26359fffe09814402dff
Angélica Campos <angelicacampos.dev@gmail.com>
2023-02-01 18:14:12 -0300

    ajustes showmenu cardCategoria

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,21 @@
-import 'package:cron/cron.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:to_file/databases/database_config.dart';
 import 'package:to_file/pages/homePage.dart';
-import 'package:to_file/services/notificacaoService.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  DatabaseHelper.instance.initDatabase();
-  final NotificationService _notificationService = NotificationService();
-  _notificationService.initializeNotifications();
-  final cron = Cron();
-  cron.schedule(
-    Schedule.parse('*/15 * * * * *'),
-    () async => {
-      await _notificationService.mostrarNotificacoes(),
-    },
-  );
+  // WidgetsFlutterBinding.ensureInitialized();
+  // DatabaseHelper.instance.initDatabase();
+  // final NotificationService _notificationService = NotificationService();
+  // _notificationService.initializeNotifications();
+  // final cron = Cron();
+  // cron.schedule(
+  //   Schedule.parse('*/15 * * * * *'),
+  //   () async => {
+  //     await _notificationService.mostrarNotificacoes(),
+  //   },
+  // );
   runApp(const MyApp());
 }
-
-// void main() {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   DatabaseHelper.instance.initDatabase();
-//   final NotificationService _notificationService = NotificationService();
-//   _notificationService.initializeNotifications();
-//   final cron = Cron();
-//   cron.schedule(
-//     Schedule.parse('*/15 * * * * *'),
-//     () async => {
-//       await _notificationService.mostrarNotificacoes(),
-//     },
-//   );
-//   runApp(const MyApp());
-// }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
