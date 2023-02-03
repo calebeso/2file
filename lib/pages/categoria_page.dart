65fc0f1587843279e8be105385d2677a8b3f5180
calebeso <calebeso045@gmail.com>
2023-02-03 20:32:51 -0300

    hotfix : voltando pagina de categoria

--- lib/pages/categoria_page.dart
+++ lib/pages/categoria_page.dart
@@ -60,159 +60,155 @@ class _CategoriaPageState extends State<CategoriaPage> {
         child: FutureBuilder<List<Documento>>(
           future: getDocs(),
           builder: (context, snapshot) {
-            return snapshot.data!.isNotEmpty
-                ? ListView(
-                    children: snapshot.data!.map((document) {
-                      return Center(
-                        child: GestureDetector(
-                          onTap: () => print('teste'),
-                          child: Card(
-                            color: const Color(0xffDEF1EB),
-                            child: Column(
-                              children: [
-                                ListTile(
-                                  contentPadding:
-                                      const EdgeInsets.fromLTRB(10, 10, 10, 5),
-                                  leading: Image.asset(
-                                      'assets/images/icon_doc.png',
-                                      height: 60),
-                                  title: Text(
-                                    document.nome,
-                                    style: const TextStyle(
+            if (snapshot.hasData) {
+              return ListView(
+                children: snapshot.data!.map((document) {
+                  return Center(
+                    child: GestureDetector(
+                      onTap: () => print('teste'),
+                      child: Card(
+                        color: const Color(0xffDEF1EB),
+                        child: Column(
+                          children: [
+                            ListTile(
+                              contentPadding:
+                                  const EdgeInsets.fromLTRB(10, 10, 10, 5),
+                              leading: Image.asset('assets/images/icon_doc.png',
+                                  height: 60),
+                              title: Text(
+                                document.nome,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 24),
+                              ),
+                              subtitle: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: RichText(
+                                  text: TextSpan(children: <TextSpan>[
+                                    const TextSpan(
+                                      text: 'Competência: ',
+                                      style: TextStyle(
                                         fontWeight: FontWeight.bold,
-                                        fontSize: 24),
-                                  ),
-                                  subtitle: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: RichText(
-                                      text: TextSpan(children: <TextSpan>[
-                                        const TextSpan(
-                                          text: 'Competência: ',
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.black,
-                                          ),
-                                        ),
-                                        TextSpan(
-                                          text:
-                                              ' ${DateFormat(DateFormat.YEAR_MONTH, 'pt-Br').format(document.dataCompetencia)}.',
-                                          style: const TextStyle(
-                                              color: Colors.black),
-                                        ),
-                                        const TextSpan(
-                                            text: '\nValidade: ',
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.black)),
-                                        TextSpan(
-                                          text:
-                                              ' ${DateFormat('dd/MM/yyyy').format(document.dataValidade)}.',
-                                          style: const TextStyle(
-                                              color: Colors.black),
-                                        ),
-                                        const TextSpan(
-                                            text: '\nCriado em: ',
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.black)),
-                                        TextSpan(
-                                          text:
-                                              ' ${DateFormat('dd/MM/yyyy KK:mm').format(document.criadoEm)}',
-                                          style: const TextStyle(
-                                              color: Colors.black),
-                                        ),
-                                      ]),
+                                        color: Colors.black,
+                                      ),
                                     ),
-                                  ),
-                                  trailing: PopupMenuButton(
-                                    itemBuilder: (BuildContext context) => [
-                                      const PopupMenuItem<_ValueDialog>(
-                                        value: _ValueDialog.editar,
-                                        child: Text("Editar"),
-                                      ),
-                                      const PopupMenuItem(
-                                        value: _ValueDialog.excluir,
-                                        child: Text('Excluir'),
-                                      ),
-                                    ],
-                                    onSelected: (value) {
-                                      switch (value) {
-                                        case _ValueDialog.editar:
-                                          Navigator.pushReplacement(
-                                              context,
-                                              MaterialPageRoute(
-                                                  builder:
-                                                      (BuildContext context) =>
-                                                          EditDocumentoPage(
-                                                              documento:
-                                                                  document)));
-                                          break;
-                                        case _ValueDialog.excluir:
-                                          showDialog(
-                                            context: context,
-                                            builder: (context) => AlertDialog(
-                                              elevation: 5.0,
-                                              title: Text(
-                                                  "Deseja excluir ${document.nome} definitivamente?"),
-                                              actions: [
-                                                MaterialButton(
-                                                  child: const Text("Sim"),
-                                                  onPressed: () {
-                                                    setState(() {
-                                                      _documentoDbHelper
-                                                          .removeDocumento(
-                                                              document.id!);
-                                                    });
-                                                    ScaffoldMessenger.of(
-                                                            context)
-                                                        .showSnackBar(
-                                                            const SnackBar(
-                                                      content: Text(
-                                                          "Documento excluído com sucesso!"),
-                                                      duration:
-                                                          Duration(seconds: 2),
-                                                      backgroundColor:
-                                                          Color(0xffFE7C3F),
-                                                    ));
-                                                    _excluirNotificacao(
-                                                        document.id!);
-                                                    _deletarImagem(
-                                                        document.nome_imagem);
-                                                    Navigator.pop(context);
-                                                  },
-                                                ),
-                                                MaterialButton(
-                                                  child: const Text('Não'),
-                                                  onPressed: () {
-                                                    Navigator.pop(context);
-                                                  },
-                                                )
-                                              ],
-                                            ),
-                                          );
-                                          break;
-                                      }
-                                    },
-                                  ),
-                                  isThreeLine: false,
-                                  onTap: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (BuildContext context) =>
-                                                ImagemViewPage(
-                                                    id_documento:
-                                                        document.id!)));
-                                  },
+                                    TextSpan(
+                                      text:
+                                          ' ${DateFormat(DateFormat.YEAR_MONTH, 'pt-Br').format(document.dataCompetencia)}.',
+                                      style:
+                                          const TextStyle(color: Colors.black),
+                                    ),
+                                    const TextSpan(
+                                        text: '\nValidade: ',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black)),
+                                    TextSpan(
+                                      text:
+                                          ' ${DateFormat('dd/MM/yyyy').format(document.dataValidade)}.',
+                                      style:
+                                          const TextStyle(color: Colors.black),
+                                    ),
+                                    const TextSpan(
+                                        text: '\nCriado em: ',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black)),
+                                    TextSpan(
+                                      text:
+                                          ' ${DateFormat('dd/MM/yyyy KK:mm').format(document.criadoEm)}',
+                                      style:
+                                          const TextStyle(color: Colors.black),
+                                    ),
+                                  ]),
                                 ),
-                              ],
+                              ),
+                              trailing: PopupMenuButton(
+                                itemBuilder: (BuildContext context) => [
+                                  const PopupMenuItem<_ValueDialog>(
+                                    value: _ValueDialog.editar,
+                                    child: Text("Editar"),
+                                  ),
+                                  const PopupMenuItem(
+                                    value: _ValueDialog.excluir,
+                                    child: Text('Excluir'),
+                                  ),
+                                ],
+                                onSelected: (value) {
+                                  switch (value) {
+                                    case _ValueDialog.editar:
+                                      Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (BuildContext context) =>
+                                                  EditDocumentoPage(
+                                                      documento: document)));
+                                      break;
+                                    case _ValueDialog.excluir:
+                                      showDialog(
+                                        context: context,
+                                        builder: (context) => AlertDialog(
+                                          elevation: 5.0,
+                                          title: Text(
+                                              "Deseja excluir ${document.nome} definitivamente?"),
+                                          actions: [
+                                            MaterialButton(
+                                              child: const Text("Sim"),
+                                              onPressed: () {
+                                                setState(() {
+                                                  _documentoDbHelper
+                                                      .removeDocumento(
+                                                          document.id!);
+                                                });
+                                                ScaffoldMessenger.of(context)
+                                                    .showSnackBar(
+                                                        const SnackBar(
+                                                  content: Text(
+                                                      "Documento excluído com sucesso!"),
+                                                  duration:
+                                                      Duration(seconds: 2),
+                                                  backgroundColor:
+                                                      Color(0xffFE7C3F),
+                                                ));
+                                                _excluirNotificacao(
+                                                    document.id!);
+                                                _deletarImagem(
+                                                    document.nome_imagem);
+                                                Navigator.pop(context);
+                                              },
+                                            ),
+                                            MaterialButton(
+                                              child: const Text('Não'),
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                            )
+                                          ],
+                                        ),
+                                      );
+                                      break;
+                                  }
+                                },
+                              ),
+                              isThreeLine: false,
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (BuildContext context) =>
+                                            ImagemViewPage(
+                                                id_documento: document.id!)));
+                              },
                             ),
-                          ),
+                          ],
                         ),
-                      );
-                    }).toList(),
-                  )
-                : const Center(child: Text('Sem documentos!'));
+                      ),
+                    ),
+                  );
+                }).toList(),
+              );
+            } else {
+              return const CircularProgressIndicator();
+            }
           },
         ),
       ),
